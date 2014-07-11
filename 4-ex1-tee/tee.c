55ef5ddd078a6f7624b3310031822b8744dc7637
Toni Schmidbauer <toni@stderr.at>
2014-07-11 08:21:18 +0200

    more work on tee

--- 4-ex1-tee/tee.c
+++ 4-ex1-tee/tee.c
@@ -1,11 +1,24 @@
+#include <sys/stat.h>
 #include <tlpi_hdr.h>
+
+#define MAX_READ 1024
 
 int main(int argc, char *argv[])
 {
+
+  char buffer[MAX_READ+1];
+
   if (argc < 1)
     usageErr("%s file", argv[0]);
 
+  /* fd = open(STDIN_FILENO, O_RDONLY); */
+  /* if (fd == -1) */
+  /*   errExit("open"); */
 
+  while( (read(STDIN_FILENO, buffer, MAX_READ)) > 0) {
+    buffer[MAX_READ+1] = '\0';
+    printf("%s", buffer);
+  }
 
-  return 1;
+  return 0;
 }
